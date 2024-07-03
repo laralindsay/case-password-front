324a0e2910cf70a147029635932b234d98f3204c
Lara Lindsay <laralindsay00@outlook.com>
2024-07-03 19:28:57 -0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideRouter } from '@angular/router';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    provideAnimations(),
+    provideToastr(),
+    provideHttpClient(withFetch()), provideAnimationsAsync()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
+import { AuthGuard } from './services/auth-guard.service';
+import { LoginComponent } from './pages/login/login.component';
 import { Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
+},
+{
+    path: "user",
+    component: UserComponent,
+    canActivate: [AuthGuard]
+}
+];
